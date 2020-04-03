17b049e3990bed57036d192a0a5bfdaeff5b6663
Alcides Mignoso <alcidesmig@gmail.com>
2020-04-03 19:56:10 +0000

    Retirando comentários desnecessários

--- puc_goias_JCECEC_2019/c.cpp
+++ puc_goias_JCECEC_2019/c.cpp
@@ -15,11 +15,9 @@ bool in(string & x, string & y) {
 	if(x == y) return true;
 	int size_x = x.size();
 	for(int i = 0; i < y.size() - size_x + 1; i++) {
-		// cout << "entrou for " << i << " " << size_x << " " << y << endl;
 		if(x == y.substr(i, size_x)){
 			return true;
 		}
-		// cout << "saiu for" << endl;
 	}	
 	return false;
 }
@@ -40,29 +38,26 @@ int main() {
 		 	cin >> V[i];
 		}
 		for (int i = 0; i < s.size(); i++) {
-			// cout << "I: " << i << " " << s.size() << endl;
 			for(int j = 1; j < s.size() - i + 1; j++) {
 				string current = s.substr(i, j);
-				// cout << "Current: " << current << endl;
+				
 				flag = true;
 				if(palindromo(current)) {
-					// cout << "Palindromo! " << current << endl;
 					current_size = current.size();
 					for (int k = 0; k < m; k++) {
 						if(current_size < V[k].size()) {
-							// cout << "Current_size < vi_size: " << current << " - " << V[k] << endl;
 							continue;
 						}
-						// cout << "entrou " << V[k] << "-" << current << endl;
+						
 						if(in(V[k], current)) {
 							flag = false;
 							break;
 						}
-						// cout << "passou" << endl;
+						
 					}
 					if(flag) {
 						cont++;
-						// cout << "Adicionado! -> " << current << endl;
+					
 					}
 				}
 			}
